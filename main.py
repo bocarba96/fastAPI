4cbc354ee78f638b4901c57448fb775689572085
Bocar BA <babocarba96@gmail.com>
2023-11-26 20:26:58 +0000

    contenu supprime

--- main.py
+++ main.py
@@ -8,40 +8,4 @@ from model import User, Gender, Role
 app = FastAPI()
 
 
-db: List[User] = [
-    User(id=uuid4(), first_name="Bocar", last_name="BA", gender=Gender.male, roles=[Role.user])
-]
-
-
-@app.get("/hello")
-async def root():
-    return {
-        "message": "Bienvenue sur fastApi"
-    }
-
-@app.get("/api/v1/users")
-async def get_users():
-    return db
-
-
-@app.get("/api/v1/users/{user_id}")
-async def get_users(user_id: str):
-    for user in db:
-        if (str(user.id) == user_id):
-            return user
-        raise HTTPException(
-            status_code=404,
-            detail=f"User with id = {user_id} not found"
-        )
-    return {
-        "message":"Aucun user dans la base de donnee"
-    }
-
-@app.post("/api/v1/users")
-async def post_users(user: User):
-    db.append(user);
-    raise HTTPException(
-        status_code=200,
-        detail="OK"
-    )
- 
+# contenu supprime
